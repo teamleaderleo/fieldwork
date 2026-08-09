ee98e19d28219521784307c8a0d2d47b4166bec7
Leo <cheerleaderleo@outlook.com>
2026-08-09 10:01:13 +0800

    Godot: add external-move autoload startup probe

---
+++ playgrounds/EXP-20260809-godot-uid-external-autoload/godot/main.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+func _ready() -> void:
+	var root := get_tree().root
+	var has_moved := root.has_node("Moved")
+	var resolved := ResourceUID.uid_to_path("uid://60nf3")
+	print("AUTOLOAD_STARTUP_RESULT has_moved=%s resolved_uid_path=%s moved_file_exists=%s old_file_exists=%s" % [
+		str(has_moved),
+		resolved,
+		str(FileAccess.file_exists("res://moved/autoload.gd")),
+		str(FileAccess.file_exists("res://old/autoload.gd")),
+	])
+	get_tree().quit(0 if has_moved else 2)
