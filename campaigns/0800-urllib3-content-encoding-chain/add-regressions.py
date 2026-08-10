14f77d3799a8e0973ffaccaa0c6056ce4ebda7b3
Leo <cheerleaderleo@outlook.com>
2026-08-11 03:36:41 +0800

    experiment: add HTTP empty-list controls to urllib3 regressions

--- campaigns/0800-urllib3-content-encoding-chain/add-regressions.py
+++ campaigns/0800-urllib3-content-encoding-chain/add-regressions.py
@@ -16,45 +16,38 @@ def test_fieldwork_unknown_content_encoding_chain_stays_opaque() -> None:
     msg = b"fieldwork-unknown-chain"
 
     cases = [
-        ("x-fieldwork", zlib.compress(msg), zlib.compress(msg)),
-        (
-            "gzip, x-fieldwork",
-            zlib.compress(gzip.compress(msg)),
-            zlib.compress(gzip.compress(msg)),
-        ),
-        (
-            "x-fieldwork, gzip",
-            gzip.compress(zlib.compress(msg)),
-            gzip.compress(zlib.compress(msg)),
-        ),
-        ("gzip,", zlib.compress(gzip.compress(msg)), zlib.compress(gzip.compress(msg))),
+        ("x-fieldwork", zlib.compress(msg)),
+        ("gzip, x-fieldwork", zlib.compress(gzip.compress(msg))),
+        ("x-fieldwork, gzip", gzip.compress(zlib.compress(msg))),
     ]
 
-    for content_encoding, wire, expected in cases:
+    for content_encoding, wire in cases:
         response = HTTPResponse(
             BytesIO(wire),
             headers={"content-encoding": content_encoding},
             preload_content=False,
         )
-        assert response.read() == expected
+        assert response.read() == wire
 
 
 def test_fieldwork_supported_content_encoding_chains_still_decode() -> None:
     msg = b"fieldwork-supported-chain"
 
-    gzip_deflate = HTTPResponse(
-        BytesIO(zlib.compress(gzip.compress(msg))),
-        headers={"content-encoding": "gzip, deflate"},
-        preload_content=False,
-    )
-    assert gzip_deflate.read() == msg
+    cases = [
+        ("gzip, deflate", zlib.compress(gzip.compress(msg))),
+        ("deflate, deflate", zlib.compress(zlib.compress(msg))),
+        ("gzip,", gzip.compress(msg)),
+        (", gzip", gzip.compress(msg)),
+        ("gzip, , deflate", zlib.compress(gzip.compress(msg))),
+    ]
 
-    deflate_deflate = HTTPResponse(
-        BytesIO(zlib.compress(zlib.compress(msg))),
-        headers={"content-encoding": "deflate, deflate"},
-        preload_content=False,
-    )
-    assert deflate_deflate.read() == msg
+    for content_encoding, wire in cases:
+        response = HTTPResponse(
+            BytesIO(wire),
+            headers={"content-encoding": content_encoding},
+            preload_content=False,
+        )
+        assert response.read() == msg
 '''
 
 path.write_text(source)
